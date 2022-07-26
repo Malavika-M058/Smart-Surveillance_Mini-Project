efb641b80fa66883c6e911e0bff132a15434225d
Aiswarya Sankar <71959577+aiswaryasankar23@users.noreply.github.com>
2022-07-26 21:55:57 +0530

    Update record.py

--- record.py
+++ record.py
@@ -2,23 +2,23 @@ import cv2
 from datetime import datetime
 
 def record():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0) # for webcam
 
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(f'recordings/{datetime.now().strftime("%H-%M-%S")}.avi', fourcc,20.0,(640,480))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID') # a four char string converted into interger form,( xvid to compress the video without loosing quality 
+    out = cv2.VideoWriter(f'recordings/{datetime.now().strftime("%H-%M-%S")}.avi', fourcc,20.0,(640,480)) # how the video is stored (path ,the storage format) 
 
     while True:
-        _, frame = cap.read()
+        _, frame = cap.read() #first frame captured
 
         cv2.putText(frame, f'{datetime.now().strftime("%D-%H-%M-%S")}', (50,50), cv2.FONT_HERSHEY_COMPLEX,
-                        0.6, (255,255,255), 2)
+                        0.6, (255,255,255), 2) # to show the date and time onscreen , font for displaying them
 
-        out.write(frame)
+        out.write(frame) 
         
 
-        cv2.imshow("esc. to stop", frame)
+        cv2.imshow("esc. to stop", frame) #click esc 
 
-        if cv2.waitKey(1) == 27:
+        if cv2.waitKey(1) == 27: # value of esc
             cap.release()
             cv2.destroyAllWindows()
             break 
